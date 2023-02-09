af6a7a9be6aa890b0176f8be2b6a178acc16372b
Irina Vodopian <ivodopian@lenovo.com>
2023-02-09 15:42:18 +0100

    v 1.7 swagger is implemented(no api are exposed)

--- Bookshop/src/main/java/com/bookshop/main/Application.java
+++ Bookshop/src/main/java/com/bookshop/main/Application.java
@@ -2,8 +2,11 @@ package com.bookshop.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 
 @SpringBootApplication
+@Import(SpringDataRestConfiguration.class)
 public class Application {
 
 	public static void main(String[] args) {
